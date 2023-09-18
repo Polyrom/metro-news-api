717ea3fc21b006ce887357f3c7e1bbbda5cd99f8
Roman Poliakov <roman61394@gmail.com>
2023-09-18 03:53:46 +0300

    update deprecated pydantic API handlers

--- api/schemas.py
+++ api/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, NaiveDatetime
+from pydantic import BaseModel, NaiveDatetime, ConfigDict, field_serializer
 
 from api.utils import convert_date_to_ymd
 
@@ -8,8 +8,8 @@ class NewsItemBase(BaseModel):
     image_url: str | None
     date_published: NaiveDatetime
 
-    class Config:
-        from_attributes = True
-        json_encoders = {
-            NaiveDatetime: convert_date_to_ymd
-        }
+    @field_serializer('date_published')
+    def serialize_date_published(self, dt: NaiveDatetime, _info):
+        return convert_date_to_ymd(dt)
+
+    model_config = ConfigDict(from_attributes=True)
